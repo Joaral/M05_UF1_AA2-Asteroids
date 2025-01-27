67837fce6151e9e805d7cc752fa1279816b29f08
Joaral <joan.ariza@enti.cat>
2025-01-27 13:40:07 +0100

    Actividades practicas hechas

--- Asteroids-Incomplete-main/Assets/Scripts/MovimientoAleatorio.cs
+++ Asteroids-Incomplete-main/Assets/Scripts/MovimientoAleatorio.cs
@@ -1,12 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 
 public class MovimientoAleatorio : MonoBehaviour
 {
+    public float min_force = -50;
+    public float max_force = 50;
     public Rigidbody2D rb;
     void Start()
     {
-
+        rb = GetComponent<Rigidbody2D>();
+        rb.AddForce(new Vector3(Random.Range(min_force, max_force), Random.Range(min_force, max_force)));
+        transform.eulerAngles += new Vector3(0, 0, Random.Range(min_force, max_force));
     }
 }
